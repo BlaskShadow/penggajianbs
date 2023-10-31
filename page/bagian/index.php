655f216e79168e322d15d613046b7d61292ed527
ZIDAnE Database <omnomercy@gmail.com>
2023-11-01 07:56:05 +0800

    DONE

--- page/bagian/index.php
+++ page/bagian/index.php
@@ -15,31 +15,54 @@ include '../../templates/navbar.php';
 ?>
 
 <body>
+    <h2 style="margin: 20px 0px; text-align:center;">Data Bagian</h2>
+
     <!--card-->
-    <section id="card">
-        <div class="container">
-                
+    <section class="container">
+        <div class="card">
+            <div class="card-header">
+                <a href="tambah.php" class="btn bg-primary" style="color: white; margin-left: 10px;">Tambah</a>
+            </div>
+            <div class="card-body">
+                <table class="table" id="example" class="table table-striped">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Opsi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $no = 1;
+                        $bagian = $koneksi->query("SELECT * FROM bagian");
+                        while ($data = $bagian->fetch_array()) {
+                        ?>
+                            <tr>
+                                <td><?= $no++; ?></td>
+                                <td><?= $data['id'] ?></td>
+                                <td><?= $data['nama'] ?></td>
+                                <td>
+                                    <a href="edit.php?id=<?= $data['id'] ?>" class="btn bg-success" style="color: white;">Edit</a>
+                                    <a href="proses.php?id=<?= $data['id'] ?>" class="btn bg-danger" style="color: white;">Hapus</a>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </section>
-    <?php
-    include '../../templates/footer.php';
-    ?>
-
-    <?php
-    include '../../templates/script.php';
-    ?>
-
-    <?php
-    include '../../templates/script.php';
-    ?>
-    <!--end footer-->
     <!--End card-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
+    <?php
+    include '../../templates/script.php';
+    ?>
 </body>
+
 </html>
+
+<script>
+    new DataTable('#example');
+</script>
